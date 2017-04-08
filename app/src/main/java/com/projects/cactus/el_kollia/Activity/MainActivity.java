475e351219535bca527c974380cc90512d4ea4ec
BishoyAbd <BishoyAbd@gmail.com>
2017-04-08 15:57:39 +0200

    copy rights added

--- app/src/main/java/com/projects/cactus/el_kollia/Activity/MainActivity.java
+++ app/src/main/java/com/projects/cactus/el_kollia/Activity/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by Bishoy on 4/8/2017.
+ * Created by Bishoy  on 4/8/2017.
  */
 
 public class MainActivity extends AppCompatActivity{
