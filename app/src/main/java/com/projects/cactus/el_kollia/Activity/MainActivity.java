3c668c088b29a649b876a8ba02f792aa54479706
BishoyAbd <BishoyAbd@gmail.com>
2017-04-08 15:46:40 +0200

    v1.1

--- app/src/main/java/com/projects/cactus/el_kollia/Activity/MainActivity.java
+++ app/src/main/java/com/projects/cactus/el_kollia/Activity/MainActivity.java
@@ -1,10 +1,8 @@
 package com.projects.cactus.el_kollia.Activity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ViewGroup;
 
 /**
  * Created by el on 4/8/2017.
